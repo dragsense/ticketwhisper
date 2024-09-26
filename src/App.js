bebc811109c96a2eac384ddd59b9d83e30b5c07e
Rana Awais <awaiswork803@gmail.com>
2024-09-26 11:14:05 +0500

    ..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 //import logo from "./logo.svg";
 import "./styles.css";
 import "./App.css";
@@ -10,8 +9,8 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
+          {/* Route for LandingPage */}
           <Route path="/" element={<LandingPage />} />
-          <LandingPage />
         </Routes>
       </div>
     </Router>
